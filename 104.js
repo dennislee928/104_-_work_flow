c200462aedb1e9a7f2e064400de241163c4d927b
dennislee928 <pcleegood@gmail.com>
2025-01-10 13:39:41 +0800

    init

---
+++ 104.js
@@ -0,0 +1,33 @@
+const url =
+  "https://apis.104api.com.tw/prohrm/1.0/hrmapi/external/transferCard";
+const token = "your_access_token";
+
+const headers = {
+  "Content-Type": "application/json",
+  Authorization: `Bearer ${token}`,
+};
+
+const body = JSON.stringify([
+  {
+    empNo: "119008",
+    cardTime: 1566349095938,
+  },
+]);
+
+fetch(url, {
+  method: "POST",
+  headers: headers,
+  body: body,
+})
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return response.json();
+  })
+  .then((data) => {
+    console.log("Response data:", data);
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+  });
